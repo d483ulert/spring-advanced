37eff8e4be11ba0784c7b8da04a2814231366e96
Kim Jisung <49260265+d483ulert@users.noreply.github.com>
2023-06-02 21:40:20 +0900

    동시성 테스트
    Signed-off-by: jisung7153 <jisung.kim@mobin-inc.com>

---
+++ src/test/java/spring/advanced/threadlocal/FieldServiceTest.java
@@ -0,0 +1,48 @@
+package spring.advanced.threadlocal;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+public class FieldServiceTest {
+
+    private FieldService fieldService = new FieldService();
+
+
+    @Test
+    void field(){
+        log.info("main test");
+        Runnable userA= () ->{
+            fieldService.logic("userA");
+        };
+        Runnable userB= () ->{
+            fieldService.logic("userB");
+        };
+
+
+        Thread threadA = new Thread(userA);
+        Thread threadB = new Thread(userB);
+
+        threadA.setName("Thread-A");
+        threadB.setName("Thread-B");
+
+        threadA.start();
+        sleep(100);    //동시성 발생X
+        threadB.start();
+
+        sleep(3000);
+        log.info("main exit");
+    }
+
+
+    private void sleep(int mils){
+        try {
+            Thread.sleep(mils);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
